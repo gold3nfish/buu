4cbe25cef9a05f9e27344f6c92baf381b12badbe
Taweewat Phonksawai <gold3nfish@me.com>
2025-03-12 20:47:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ app.use(express.urlencoded({ extended: true }));
 
 // Database configuration
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 3306,
-  database: process.env.DB_NAME || 'qr_code',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'FTOedf36275'
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
 });
 
 // Check database connection
